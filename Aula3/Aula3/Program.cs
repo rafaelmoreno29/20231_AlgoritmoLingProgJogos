b3e8998ef3f2a5cfa966dd7db659af8a4922bce7
Rafael Moreno <rafaelmorenosax@gmail.com>
2023-03-09 10:14:33 -0300

    Inicial

---
+++ Aula3/Aula3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string tipo;
+            double qtdeEntrada, qtdeSaida;
+
+            Console.WriteLine("Digite o Tipo [M ou F]");
+            tipo = Console.ReadLine();
+            Console.WriteLine("Digite a quantidade");
+            qtdeEntrada = Convert.ToDouble(Console.ReadLine());
+
+            if(tipo == "M")
+            {
+                qtdeSaida = 200 * qtdeEntrada / 500;
+                Console.WriteLine("A qtde de frios é: " + qtdeSaida);
+            }
+            else
+            {
+                qtdeSaida = 500 * qtdeEntrada / 200;
+                Console.WriteLine("A qtde de manteiga é: " + qtdeSaida);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Exemplo1/Exemplo1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemplo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, r;
+
+            Console.WriteLine("Digite a base");
+            b = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite a altura");
+            a = Convert.ToDouble(Console.ReadLine());
+
+            r = b * a;
+
+            //Math.Pow((a+b), 2);
+
+            Console.WriteLine("A área é: " + r);
+
+            Console.WriteLine("Digite uma tecla para finalizar...");
+            Console.ReadKey();
+        }
+    }
+}
