4fad709528446b67ee78c853c16dd012b94e4f19
Rafael Moreno <rafaelmorenosax@gmail.com>
2023-03-09 11:13:10 -0300

    exercicios

---
+++ Aula3Ex7/Aula3Ex7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula3Ex7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double distancia, litros, media;
+            Console.WriteLine("Digite a distância");
+            distancia = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite a qtde de litros");
+            litros = Convert.ToDouble(Console.ReadLine());
+
+            media = distancia / litros;
+
+            Console.WriteLine("A média é: " + media);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Aula3Ex8/Aula3Ex8/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula3Ex8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, resultado;
+            Console.WriteLine("Digite o número 1");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite o número 2");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            if(num1 > num2)
+                resultado = num1 - num2;
+            else
+                resultado = num2 - num1;
+
+            Console.WriteLine(resultado);
+            Console.ReadKey();
+        }
+    }
+}
